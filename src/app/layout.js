e25bed3e5a604207122513a4242439726c7beaad
Curtis Witter <inward-panting0f@icloud.com>
2025-02-25 21:57:10 +0000

    import providers to layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Providers } from "@/Providers";
 
 const inter = Inter({
   subsets: ["latin"]
@@ -15,8 +16,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body
         className={`${inter.className} `}
-      >
-        {children}
+      > <Providers>{children}</Providers>
       </body>
     </html>
   );
